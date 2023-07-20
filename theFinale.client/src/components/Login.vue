f319c8140642959df4b7bddb2e3ac254e41ad936
Nathan Moehlmann <nathan.moehlmann@gmail.com>
2023-07-20 09:12:29 -0600

    asdf

--- theFinale.client/src/components/Login.vue
+++ theFinale.client/src/components/Login.vue
@@ -6,7 +6,7 @@
     </button>
     <div v-else class="d-flex p-2">
       <div class="dropdown my-lg-0">
-        <div type="button" class="border-0 selectable no-select" data-bs-toggle="dropdown" aria-expanded="false">
+        <div type="button" class="border-0 selectable no-select" data-bs-toggle="dropdown">
           <div v-if="account.picture || user.picture">
             <img :src="account.picture || user.picture" alt="account photo" height="40" class="rounded" />
           </div>
